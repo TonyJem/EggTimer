44c8bb58e0748283bd73cf1e536553975fec8c7b
TonyJem <62282734+TonyJem@users.noreply.github.com>
2020-09-21 12:54:13 +0300

    Create and setup "Preferences" Window

---
+++ EggTimer/PrefsViewController.swift
@@ -0,0 +1,17 @@
+//
+//  PrefsViewController.swift
+//  EggTimer
+//
+//  Created by Anton on 2020-09-21.
+//
+
+import Cocoa
+
+class PrefsViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}

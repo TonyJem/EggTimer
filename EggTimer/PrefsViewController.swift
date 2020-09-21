9b17b718822a0b20cd355dc7e322255ebe98bcd1
TonyJem <62282734+TonyJem@users.noreply.github.com>
2020-09-21 13:03:32 +0300

    Add Outlets and Actions for elements in "Preferences" view

--- EggTimer/PrefsViewController.swift
+++ EggTimer/PrefsViewController.swift
@@ -9,9 +9,24 @@ import Cocoa
 
 class PrefsViewController: NSViewController {
 
+    @IBOutlet var presetsPopup: NSPopUpButton!
+    @IBOutlet var customSlider: NSSlider!
+    @IBOutlet var customTextField: NSTextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do view setup here.
     }
     
+    @IBAction func popupValueChanged(_ sender: Any) {
+    }
+    
+    @IBAction func sliderValueChanged(_ sender: Any) {
+    }
+    
+    @IBAction func cancelButtonClicked(_ sender: Any) {
+    }
+    
+    @IBAction func okButtonClicked(_ sender: Any) {
+    }
 }

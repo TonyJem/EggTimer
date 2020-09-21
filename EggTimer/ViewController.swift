221c3f136f3f2349417db6d285ae6f4a87d0f21e
TonyJem <62282734+TonyJem@users.noreply.github.com>
2020-09-21 09:48:46 +0300

    Create Outlets and actions for elements in MainWindow UI

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -9,6 +9,14 @@ import Cocoa
 
 class ViewController: NSViewController {
 
+    
+    @IBOutlet var timeLeftField: NSTextField!
+    @IBOutlet var eggImageView: NSImageView!
+    @IBOutlet var startButton: NSButton!
+    @IBOutlet var stopButton: NSButton!
+    @IBOutlet var resetButton: NSButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -21,6 +29,15 @@ class ViewController: NSViewController {
         }
     }
 
+    @IBAction func startButtonClicked(_ sender: Any) {
+    }
+    
+    @IBAction func stopButtonClicked(_ sender: Any) {
+    }
+    
+    @IBAction func resetButtonClicked(_ sender: Any) {
+    }
+    
 
 }
 
